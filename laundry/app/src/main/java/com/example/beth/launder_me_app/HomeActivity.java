38532c3cdba28e5d0fb9a1cc7dc3a633b3c96e93
BethMwangi <wanjikumwangib@gmail.com>
2016-05-29 23:08:57 +0300

    laundry app with splash and home activity

---
+++ laundry/app/src/main/java/com/example/beth/launder_me_app/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.beth.launder_me_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by beth on 5/29/16.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
